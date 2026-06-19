699cd20715071750c189a39f5c44f91404e18d7e
fjdlkjf <aryan797042@gmail.com>
2026-06-19 14:14:32 +0530

    Initial commit - Student Management System with exception handling

---
+++ new.py
@@ -0,0 +1,9 @@
+name = input("Enter your name:")
+
+print (f"{name}  ->  {type(name)}")
+
+
+
+# bitwise operator 
+
+print (4 & 3)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+
+
+# a = "this is single line"
+
+# print(a)
+
+b = "This is single line" \
+"            and " \
+"    and this is second line   "
+
+print(b)
+
+
